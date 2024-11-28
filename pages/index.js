bbcc135de9887c7d876a196b1dbb5012f5b4cea9
Gabriela Cavalcanti <99495563+Gabriela-LC@users.noreply.github.com>
2024-11-28 20:57:47 +0000

    adiciona pasta pages com página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){ //D4 - H maiúsculo que é aconvenção do React para componentes
+    return <h1>huhu</h1>
+}
+
+export default Home
+// D4 - cola entre next e react
+// D4 - export default - como sinalizamos para o next js que é a função Home que queremos usar para renderizar a página sendo que neste arquivo pode haver diversas funções
