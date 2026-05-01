2e5255f05227efa326346234208126231f626023
Gabriela Cavalcanti <99495563+Gabriela-LC@users.noreply.github.com>
2026-05-01 21:55:52 +0000

    texto da home atualizado

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home(){ //D4 - H maiúsculo que é aconvenção do React para componentes
-    return <h1>Projeto para curso.dev</h1>
+    return <h1>Oi Moziiii</h1>
 }
 
 export default Home
 // D4 - cola entre next e react
-// D4 - export default - como sinalizamos para o next js que é a função Home que queremos usar para renderizar a página sendo que neste arquivo pode haver diversas funções
+// D4 - export default - como sinalizamos para o next js que é a função Home 
+// que queremos usar para renderizar a página sendo que neste arquivo pode haver 
+// diversas funções
