768e1149e52116c94e038c50dad6e800380497e5
Gabriela Cavalcanti <99495563+Gabriela-LC@users.noreply.github.com>
2026-06-10 16:29:52 +0000

    fix minor issue on /migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,7 +5,7 @@ import database from "infra/database";
 export default async function migrations(req, res) {
     const allowedMethods = ["GET", "POST"];
     if (!allowedMethods.includes(req.method)) {
-        return response.status(405).json({
+        return res.status(405).json({
             error: `Method "${req.method}" not allowed`,
         });
     }
